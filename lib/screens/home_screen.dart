6f3e50c478510ac120f83b6197436820dd8d2c7a
Naufal A.N. <92589371+NaufalAndriantoNugraha@users.noreply.github.com>
2026-01-28 12:50:25 +0700

    Fitur: Tambah paket mobile_scanner

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const String routeName = '/home_screen';
+
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Hello, World!')));
+  }
+}
